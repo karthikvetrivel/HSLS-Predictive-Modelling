f802078723c65e8d42f042149fb8629dc8fd18ed
testingpagee <hunderdollarmonth@gmail.com>
2020-01-01 15:11:12 -0800

    small changes

--- missingness_imputation.py
+++ missingness_imputation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 student_data = pd.read_csv("data/HSLS_2016_v1_0_CSV_Datasets/hsls_16_student_v1_0.csv", na_values=[-9, -8, -5, -7, -4, -3]);
 student_data.head()
@@ -9,25 +10,25 @@ student_data.head()
 # -7 = Skipped
 # -4 = Question not adminstered
 # -3 = Carry through missing
-
-output_cols = ['X3CLGANDWORK', 'S3CLASSES', 'S3WORK'
-, 'S3APPRENTICE', 'X4EVRAPPCLG', 'S3CLGAPPNUM']
-output_col = student_data[output_cols]
-# Proportion of Data missing per column
-output_cols_removed = student_data.loc[:,~student_data.columns.isin(output_cols)]
-output_cols_removed_count = output_cols_removed.isna().sum()
-df = output_cols_removed_count / len(output_cols_removed)
-df.to_csv("figures/missingness_imputation_missingness_per_predictor.csv")
-
-
-# Get output_col_removed imputed with the median values
-predictor_col = output_cols_removed.fillna(output_cols_removed.median()) 
-
 # Sort by the first gathered predictor variables
-filter_col = [col for col in predictor_col if col.startswith('S1') or col.startswith('X1') or col.startswith('A1') or col.startswith('C1') or col.startswith('S2') or col.startswith('X2') or col.startswith('A2') or col.startswith('C2') or col.startswith('STU_ID')]
+
+# Getting columns that start with certain keywords
+filter_col = [col for col in student_data if col.startswith('S1') or col.startswith('X1') or col.startswith('A1') or col.startswith('C1') or col.startswith('S2') or col.startswith('X2') or col.startswith('A2') or col.startswith('C2') or col.startswith('STU_ID')]
 updated_predictor_col = student_data[filter_col] 
 
-updated_predictor_col.to_csv("data/processed/baseline_features.csv")
+updated_predictor_col_count = updated_predictor_col.isna().sum()
+df = updated_predictor_col_count / len(updated_predictor_col)
+df.to_csv("figures/missingness_imputation_missingness_per_predictor.csv")
+
+# Median Imputation
+updated_predictor_col = updated_predictor_col.fillna(updated_predictor_col.median()) 
+updated_predictor_col.head()
+
+# Removing columsn with ALL missing values
+final_predictor_col = updated_predictor_col.dropna(axis=1)
+
+# Export to CSV file
+final_predictor_col.to_csv("data/processed/baseline_features.csv")
 
 
 
--- nnet_train.py
+++ nnet_train.py
@@ -2,27 +2,37 @@ import tensorflow as tf
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+import argparse
 
 
 baseline_features = pd.read_csv("data/processed/baseline_features.csv", index_col=0)
 output_columns = pd.read_csv("data/processed/output_columns.csv", index_col=0) 
 
+# 23503 x 1470 columns
 baseline_features.head()
+# 23503 x 6 columns
 output_columns.head()
+
+# TO-DO, build a model that trains on each individual output column
+
+
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--output_col', type=str, help='name of output', default=None)
+# args = parser.parse_args()
 
 # Merge into a baseline and output into a single column
 df = pd.merge(baseline_features, output_columns, on='STU_ID')
 
-
-df
 # Remove rows w/ NaN values in the output column
 df = df.dropna(axis=0, subset=output_columns.columns)
-df
+
 
 baseline_features_final = df[output_columns.columns]
 baseline_features_final.drop(['STU_ID'], axis=1)
-output_columns.final = df[baseline_features.columns] 
-output_columns.final.drop(['STU_ID'],axis=1)
+output_columns_final = df[baseline_features.columns] 
+output_columns_final.drop(['STU_ID'],axis=1)
 
-output_columns.final.head()
+output_columns_final.head()
 
