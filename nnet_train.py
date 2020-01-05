3edae0d7ebe9873d340af917b9e4e0dca8cade0a
testingpagee <hunderdollarmonth@gmail.com>
2020-01-05 08:26:09 -0800

    primary neural net code

--- nnet_train.py
+++ nnet_train.py
@@ -13,26 +13,63 @@ baseline_features.head()
 # 23503 x 6 columns
 output_columns.head()
 
-# TO-DO, build a model that trains on each individual output column
-
-
-
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--output_col', type=str, help='name of output', default=None)
-# args = parser.parse_args()
+# Specific column to be tested on.
+main_output_column = output_columns[['S3CLASSES', 'STU_ID']]
 
 # Merge into a baseline and output into a single column
-df = pd.merge(baseline_features, output_columns, on='STU_ID')
+df = pd.merge(baseline_features, main_output_column, on='STU_ID')
 
 # Remove rows w/ NaN values in the output column
-df = df.dropna(axis=0, subset=output_columns.columns)
+df = df.dropna(axis=0, subset=main_output_column.columns)
 
-
-baseline_features_final = df[output_columns.columns]
-baseline_features_final.drop(['STU_ID'], axis=1)
+# Create the x and y columns
+baseline_features_final = df[main_output_column.columns]
+baseline_features_final = baseline_features_final.drop(['STU_ID'], axis=1)
 output_columns_final = df[baseline_features.columns] 
-output_columns_final.drop(['STU_ID'],axis=1)
+output_columns_final = output_columns_final.drop(['STU_ID'],axis=1)
 
-output_columns_final.head()
+# Splitting the data set into the Training and Testing set
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(baseline_features_final, output_columns_final, test_size = 0.2)
 
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = X_train.shape[1]))
+
+# Adding the second hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train.values.tolist(), batch_size = 10, nb_epoch = 300)
+
+# Part 3 - Making the predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
